e4a40c6125a61f3c055df69cf89342989eee1517
shangye28 <shangye1218@163.com>
2023-04-14 20:25:00 +0800

    4.14

--- sshtest/src/main/java/com/example/sshtest/controller/RoleController.java
+++ sshtest/src/main/java/com/example/sshtest/controller/RoleController.java
@@ -1,24 +1,24 @@
-//package com.example.sshtest.controller;
-//
-//
-//import com.example.sshtest.dao.RoleDao;
-//import com.example.sshtest.pojo.Role;
-//import com.example.sshtest.result.R;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/role")
-//public class RoleController {
-////    @Autowired
-////    private RoleService roleService;
+package com.example.sshtest.controller;
+
+
+import com.example.sshtest.dao.RoleDao;
+import com.example.sshtest.pojo.Role;
+import com.example.sshtest.result.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/role")
+public class RoleController {
 //    @Autowired
-//    private RoleDao roleDao;
-//
+//    private RoleService roleService;
+    @Autowired
+    private RoleDao roleDao;
+
 //    /**
 //     * 查找role所有
 //     * @return
@@ -39,4 +39,4 @@
 //        else
 //            return R.FAIL();
 //    }
-//}
+}
